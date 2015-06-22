9318179d8282acf5973ac4f9bacec610b314a658
Bharat Rao <bharatgit14@gmail.com>
2015-06-22 17:08:30 -0500

    tokens

---
+++ app_tokens.php
@@ -0,0 +1,8 @@
+<?php
+
+$consumer_key='	XLBkWmMkhrcllKEZIyGnKPbJJ';
+$consumer_secret='EtsltFAzRFNafTsTnVWGwbSjnIbgCry7ostvWAJiNIbZ8sAscO';
+$user_token='448642049-WCSocyo4mAL2cRnCJdEv0vJAts9FGx9IozRU6z6H';
+$user_secret='xy0DccFAkfyBQy8WX8VL4txok8rcdlV43ZnGlEleGo6MB';
+
+?>
