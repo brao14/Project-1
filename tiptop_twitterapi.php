67c936be1d3acc289986efa8040324bcc93d8bd8
Bharat Rao <bharatgit14@gmail.com>
2015-06-23 16:13:33 -0500

    curl try 5

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -30,7 +30,7 @@ print_r($response_rawdata);
 error_log("Bharat: $response_rawdata");
 print_r($response_data);
 */
-$tweetvariable="Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area";
+$tweetvariable="Obama%20has%20trade%20talks%20about%20nuclear%20weapons";
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweetvariable);
 $result = curl_exec($curl);
 curl_close($curl);
