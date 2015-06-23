0dc3c2807f140ccbb9373562242dcb2ff948b212
Bharat Rao <bharatgit14@gmail.com>
2015-06-23 16:03:14 -0500

    curl try 2

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -30,10 +30,10 @@ print_r($response_rawdata);
 error_log("Bharat: $response_rawdata");
 print_r($response_data);
 */
-
-$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area");
+$tweetvariable="Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area";
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" + $tweetvariable);
 $result = curl_exec($curl);
-curl_close($curl);
+curl_close($tweet);
 print $result;
 
 
