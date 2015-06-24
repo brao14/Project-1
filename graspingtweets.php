b0ec6d1cc0ba47efea0c42e1e152000659324c44
Bharat Rao <bharatgit14@gmail.com>
2015-06-24 15:26:52 -0500

    grasping tweets

---
+++ graspingtweets.php
@@ -0,0 +1,32 @@
+<?php
+require 'app_tokens.php';
+require 'tmhOAuth.php';
+
+$connection = new tmhOAuth(array(
+  'consumer_key' => $consumer_key,
+  'consumer_secret' => $consumer_secret,
+  'user_token' => $user_token,
+  'user_secret' => $user_secret
+));
+
+$connection->request('GET', $connection->url('1.1/users/show'), array('screen_name' => 'washingtonpost'));
+
+$response_code=$connection->response['code'];
+
+
+$response_data = json_decode($connection->response['response'],true);
+
+$response_rawdata = $connection->response['response'];
+
+
+if ($response_code <> 200) {
+ print "Error: $response_code\n";
+ error_log("Bharat: Response code is not 200");
+}
+
+error_log("Bharat: $response_code");
+print_r($response_rawdata);
+error_log("Bharat: $response_rawdata");
+print_r($response_data);
+
+?>
