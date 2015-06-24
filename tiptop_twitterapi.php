d464845a3b157ab4672c4e4cec0e9a058e657689
Bharat Rao <bharatgit14@gmail.com>
2015-06-24 14:24:52 -0500

    change of tweet

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -30,7 +30,8 @@ print_r($response_rawdata);
 error_log("Bharat: $response_rawdata");
 print_r($response_data);
 */
-$tweetvariable="Obama%20has%20trade%20talks%20about%20nuclear%20weapons";
+// http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
+$tweetvariable="Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area";
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweetvariable);
 $result = curl_exec($curl);
 curl_close($curl);
