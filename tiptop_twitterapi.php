0a6f3fa54eab6676aed7a88355bb0f1bc814fd88
Bharat Rao <bharatgit14@gmail.com>
2015-06-24 15:26:35 -0500

    saveas

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweetvariable="Obama%20has%20trade%20talks%20about%20nuclear%20weapons";
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweetvariable);
