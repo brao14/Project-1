d39162a4a5a5237307402f446df5014cf60a317a
Bharat Rao <bharatgit14@gmail.com>
2015-06-25 16:51:50 -0500

    print statement

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -3,6 +3,7 @@
 
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweet = $_REQUEST["tweet"];
+print $tweet;
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweet);
 $result = curl_exec($curl);
 curl_close($curl);
