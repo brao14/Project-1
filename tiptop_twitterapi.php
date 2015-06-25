05bbf7bc4deb21e44bccdb26c101192cf4360271
Bharat Rao <bharatgit14@gmail.com>
2015-06-25 17:26:30 -0500

    encodedtweet

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -3,9 +3,10 @@
 
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweet = $_GET["tweet"];
-$encodedtweet = string urlencode(string $tweet);
 print $tweet;
-$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweet);
+$encodedtweet = string urlencode(string $tweet);
+print $encodedtweet;
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 $result = curl_exec($curl);
 curl_close($curl);
 //print $result;
