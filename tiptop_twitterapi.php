94ad4b35c313bf91b77eafbf9008d5c591fce620
Bharat Rao <bharatgit14@gmail.com>
2015-06-25 17:35:23 -0500

    urlencode fix

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -3,9 +3,9 @@
 
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweet = $_GET["tweet"];
-print $tweet;
-$encodedtweet = string urlencode(string $tweet);
-print $encodedtweet;
+echo ("Bharat:" . $tweet);
+$encodedtweet = urlencode($tweet);
+echo ("Bharat:" . $encodedtweet);
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 $result = curl_exec($curl);
 curl_close($curl);
