8951537c305c228783948b06c7f61d08c20adda1
Bharat Rao <bharatgit14@gmail.com>
2015-06-25 16:43:15 -0500

    identifier change

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -2,8 +2,8 @@
 
 
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
-$tweetvariable = $_REQUEST["tweet"];
-$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweetvariable);
+$tweet = $_REQUEST["tweet"];
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $tweet);
 $result = curl_exec($curl);
 curl_close($curl);
 //print $result;
