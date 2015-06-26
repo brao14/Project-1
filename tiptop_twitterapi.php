53efc5c195daa940d03693957219bd3919e08fa4
Bharat Rao <bharatgit14@gmail.com>
2015-06-26 18:42:05 -0500

    alright swag

--- tiptop_twitterapi.php
+++ tiptop_twitterapi.php
@@ -1,32 +1,15 @@
 <?php
 
-require 'app_tokens.php';
-require 'tmhOAuth.php';
 
-$connection = new tmhOAuth(array(
-  'consumer_key' => $consumer_key,
-  'consumer_secret' => $consumer_secret,
-  'user_token' => $user_token,
-  'user_secret' => $user_secret
-));
+// http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
+$tweet = $_GET["tweet"];
+//echo "<h2>$tweet</h2>";
+$encodedtweet = urlencode($tweet);
+//echo "<h2>$encodedtweet</h2>";
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
+$result = curl_exec($curl);
+curl_close($curl);
+//print $result;
+//$jsonresult = json_decode($result->response['response'], false);
 
-$connection->request('GET', $connection->url('http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area'));
-
-$response_code=$connection->response['code'];
-
-
-$response_data = json_decode($connection->response['response'],true);
-
-$response_rawdata = $connection->response['response'];
-
-
-if ($response_code <> 200) {
- print "Error: $response_code\n";
- error_log("Bharat: Response code is not 200");
-}
-
-error_log("Bharat: $response_code");
-print_r($response_rawdata);
-error_log("Bharat: $response_rawdata");
-print_r($response_data);
 ?>
