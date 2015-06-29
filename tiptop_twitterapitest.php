885810fc0b2adb8a8644c0f6bff06dc61eecbb19
Bharat Rao <bharatgit14@gmail.com>
2015-06-29 15:31:17 -0500

    fullhtml

--- tiptop_twitterapitest.php
+++ tiptop_twitterapitest.php
@@ -1,6 +1,7 @@
+<html>
+<body>
+
 <?php
-
-
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweet = $_GET["tweet"];
 //echo "<h2>$tweet</h2>";
@@ -11,16 +12,18 @@ $result = curl_exec($curl);
 curl_close($curl);
 //print $result;
 $noob = json_decode($result->response['response'], false);
-//var json = '{"bayarea": "50", "tweet": "Chilly on the first day of summer in the bay area"}'
-//var json = '{"items" : [{"tweet" : "Obama is the president of the United States", "topic" : [ {"Obama" : "50"}, {"President" : "100"} ] }, {"tweet" : "Chilly in the Bay Area", "topic" : [ {"BayArea" : "50"}, {"Weather" : "100"} ] }]}'
-//var noob = JSON.parse(json);
-//document.getElementById("tweet").innerHTML = noob.items[1].topic[1].Weather;
-//console.log("Number of topics = ", noob.topics.length);
+?>
+
+
+<table style="width:100%">
+<tr>
+<td>
+<?php echo $noob.tweet?>
+
+</td>;
+
+<?php
 $count=0;
-$table = "<table style=\"width:100%\">"
-
-$table .= "<tr><td>" . $noob.tweet . "</td>";
-
 for ($count=0; $count<$noob.topics.length; $count++)
 {
   //table += "<tr><td>" + noob.items[count].tweet + "</td>";
@@ -30,10 +33,10 @@ for ($count=0; $count<$noob.topics.length; $count++)
       $table .= "<td>" . $propertyName . ":" . $noob.topics[$count][$propertyName] . "</td>";
     }
 }
-  $table .= "</tr>";
-  //document.getElementById("tweets").innerHTML = myRows;
-  //document.getElementById("tt_main").innerHTML = myHTML;
-$table .= "</table>";
-echo($table);
-
+echo $table;
 ?>
+</tr>
+</table>
+
+</body>
+</html>
