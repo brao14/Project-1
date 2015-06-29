c729adc236f62dfc0e7db4d8a3ffa5e3d19292bb
Bharat Rao <bharatgit14@gmail.com>
2015-06-29 15:47:48 -0500

    basic

--- tiptop_twitterapitest.php
+++ tiptop_twitterapitest.php
@@ -3,21 +3,20 @@
 <?php
 // http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
 $tweet = $_GET["tweet"];
-//echo "<h2>$tweet</h2>";
+echo "<body>";
+echo "<h2>";
+echo "<h2>$tweet</h2>";
 $encodedtweet = urlencode($tweet);
-//echo "<h2>$encodedtweet</h2>";
+echo "<h2>$encodedtweet</h2>";
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 $result = curl_exec($curl);
 curl_close($curl);
 //print $result;
 $noob = json_decode($result->response['response'], false);
-echo "<html>"
-echo "<body>"
-echo "<h2>"
-echo $noob[0].tweet
-echo "</h2>"
-echo "</body>"
-echo "</html>"
+
+echo "<h2>$noob[0].tweet </h2>";
+echo "</body>";
+echo "</html>";
 
 
 /*
