d79cb9697a85cd91383cd71f9bf3bb2a2c2f13a1
Bharat Rao <bharatgit14@gmail.com>
2015-06-29 19:16:15 -0500

    basic

--- tiptop_twitterapitest.php
+++ tiptop_twitterapitest.php
@@ -7,7 +7,7 @@ echo "<html>";
 echo "<body>";
 echo "<p>Tweet passed in = $tweet</p>";
 $encodedtweet = urlencode($tweet);
-echo "<br> Encode tweet = $encodedtweet<br>"
+echo "<br> Encode tweet = $encodedtweet <br>";
 
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -15,8 +15,8 @@ $result = curl_exec($curl);
 curl_close($curl);
 echo "<p> Result from curl = $result </p>";
 
-$noob = json_decode($result);
-echo "<p> Result from json_decode  =  $noob </p>";
+//$noob = json_decode($result);
+//echo "<p> Result from json_decode  =  $noob </p>";
 //echo "<p> $noob->tweet </p>";
 
 $ar = json_decode($result, true);
