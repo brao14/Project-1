334b65b8559bce9247dd466cf53540f4a0470556
Bharat Rao <bharatgit14@gmail.com>
2015-06-29 19:07:15 -0500

    basic

--- tiptop_twitterapitest.php
+++ tiptop_twitterapitest.php
@@ -1,27 +1,27 @@
 
 
 <?php
-// http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=Chilly%20on%20the%20first%20day%20of%20summer%20in%20the%20bay%20area
+
 $tweet = $_GET["tweet"];
 echo "<html>";
 echo "<body>";
 echo "<p>Tweet passed in = $tweet</p>";
 $encodedtweet = urlencode($tweet);
-echo "<br> Encode tweet = $encodedtweet<br>";
+echo "<br> Encode tweet = $encodedtweet<br>"
+
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($curl);
 curl_close($curl);
 echo "<p> Result from curl = $result </p>";
+
 $noob = json_decode($result);
-//echo "<p> Result from json_decode =  $noob </p>";
-
+echo "<p> Result from json_decode  =  $noob </p>";
 echo "<p> $noob->tweet </p>";
-//echo "<p> $noob['tweet'] </p>";
 
-//echo "<p> count($noob['topics']) </p>";
+$ar = json_decode($result, true);
+echo "<p> Result from json_decode with true option =  $ar </p>";
+echo "<p> $ar['tweet'] </p>";
 echo "</body>";
 echo "</html>";
-
-
 ?>
