c6bba98880434316ad27219dc56e45478f7aa903
Bharat Rao <bharatgit14@gmail.com>
2015-07-06 14:31:53 -0500

    search tweets

--- graspingtweets.php
+++ graspingtweets.php
@@ -9,13 +9,11 @@ $settings = array(
     'consumer_secret' => "M6ail8PaiiLElqe4zmRWiLuZHdmevra1JjNfxTfC4w3jwvg4nu"
 );
 
-$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
 $requestMethod = 'GET';
 
 $getfields = array(
-    'user_id' => 'washingtonpost',
-    'count' => '15',
-    'include_rts' => 'false'
+    'q' => '%40washingtonpost'
 );
 
 $twitter = new TwitterAPIExchange($settings);
