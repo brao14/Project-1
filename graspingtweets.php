628f3bf89698c3626b302af2571ad1846e57129c
Bharat Rao <bharatgit14@gmail.com>
2015-07-07 16:52:39 -0500

    latest news

--- graspingtweets.php
+++ graspingtweets.php
@@ -13,7 +13,7 @@ $url = 'https://api.twitter.com/1.1/search/tweets.json';
 $requestMethod = 'GET';
 
 
-$getfields = '?q=%23news&result_type=recent&count=1';
+$getfields = '?q=news%20source%3Atwitterfeed&result_type=recent&count=1';
 
 $twitter = new TwitterAPIExchange($settings);
 
