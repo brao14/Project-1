d85fb3d4e1c287ea3e07c0c86d1c461db02582a7
Bharat Rao <bharatgit14@gmail.com>
2015-07-07 16:05:39 -0500

    washingtonpost timeline

--- graspingtweets.php
+++ graspingtweets.php
@@ -9,14 +9,11 @@ $settings = array(
     'consumer_secret' => "M6ail8PaiiLElqe4zmRWiLuZHdmevra1JjNfxTfC4w3jwvg4nu"
 );
 
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 $requestMethod = 'GET';
 
 
-$getfields = array(
-  'q'=>'@washingtonpost',
-  'result_type'=>'popular'
-);
+$getfields = '?screen_name=washingtonpost';
 //'?q=%40washingtonpost';
 
 $twitter = new TwitterAPIExchange($settings);
