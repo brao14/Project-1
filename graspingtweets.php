2e5d127bd16f93a55a214c143434ca74c28f38b1
Bharat Rao <bharatgit14@gmail.com>
2015-07-06 19:07:28 -0500

    parsing json

--- graspingtweets.php
+++ graspingtweets.php
@@ -23,6 +23,10 @@ $twitterjson =  $twitter->setGetfield($getfields)
 
 $tjd = json_decode($twitterjson);
 
+cho "<table border=\"1\" color=\"red\" style=\"width:100%\">";
+echo "<tr>";
+echo "<th> Tweet</th>";
+echo "</tr>";
 foreach ( $tjd as $k => $v )
 {
    if ( $k == "statuses")
@@ -32,13 +36,16 @@ foreach ( $tjd as $k => $v )
       foreach ( $v[$count] as $key => $val)
       {
         if ( $key == "text") {
+          echo "<tr><td>"
           echo $val;
+          echo "</td></tr>";
         }
       }
     }
   }
 
 }
+echo "</table>";
 
 /*
 
