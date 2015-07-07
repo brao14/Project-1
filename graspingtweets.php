5ec429f7ebcb58300eb52026c43e60421e04dae3
Bharat Rao <bharatgit14@gmail.com>
2015-07-07 16:32:14 -0500

    economist

--- graspingtweets.php
+++ graspingtweets.php
@@ -13,7 +13,7 @@ $url = 'https://api.twitter.com/1.1/search/tweets.json';
 $requestMethod = 'GET';
 
 
-$getfields = '?q=%40FT&result_type=popular';
+$getfields = '?q=%40TheEconomist&result_type=popular';
 
 $twitter = new TwitterAPIExchange($settings);
 
