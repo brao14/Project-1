3b9d14c6ec62b0db67e872088473574e6d5b5f23
Bharat Rao <bharatgit14@gmail.com>
2015-07-08 15:36:47 -0500

    graspingtweets.php

--- graspingtweets.php
+++ graspingtweets.php
@@ -10,11 +10,13 @@ $settings = array(
     'consumer_secret' => "M6ail8PaiiLElqe4zmRWiLuZHdmevra1JjNfxTfC4w3jwvg4nu"
 );
 
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
+//$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 $requestMethod = 'GET';
 
 
-$getfields = '?q=%40washingtonpost&result_type=recent&count=1';
+//$getfields = '?q=%40washingtonpost&result_type=recent&count=1';
+$getfields = '?screen_name=washingtonpost&include_rts=false&count=1';
 
 $twitter = new TwitterAPIExchange($settings);
 
