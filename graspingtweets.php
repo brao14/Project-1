044aecd6a7b54b2de3286fb52055e453cba6e527
Bharat Rao <bharatgit14@gmail.com>
2015-07-08 17:23:46 -0500

    graspingtweets.php

--- graspingtweets.php
+++ graspingtweets.php
@@ -10,13 +10,13 @@ $settings = array(
     'consumer_secret' => "M6ail8PaiiLElqe4zmRWiLuZHdmevra1JjNfxTfC4w3jwvg4nu"
 );
 
-//$url = 'https://api.twitter.com/1.1/search/tweets.json';
-$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+//$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 $requestMethod = 'GET';
 
 
-//$getfields = '?q=%40washingtonpost&result_type=recent&count=1';
-$getfields = '?screen_name=washingtonpost&include_rts=false&count=1';
+$getfields = '?q=%40washingtonpost&result_type=recent&count=1';
+//$getfields = '?screen_name=washingtonpost&include_rts=false&count=1';
 
 $twitter = new TwitterAPIExchange($settings);
 
@@ -34,19 +34,18 @@ echo "</tr>";
 */
 foreach ( $tjd as $k => $v )
 {
-   //if ( $k == "statuses")
-   if ($k == " ")
+   if ( $k == "statuses")
    {
     for ( $count = 0; $count < sizeof($v); $count ++ )
     {
-      //foreach ( $v[$count] as $key => $val)
-      //{
+      foreach ( $v[$count] as $key => $val)
+      {
         if ( $key == "text")
         {
           return $val;
           //echo "<br>";
         }
-      //}
+      }
     }
    }
 
