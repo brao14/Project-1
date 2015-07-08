d25a4497ee9650bd0538b575aadc475afb72d002
Bharat Rao <bharatgit14@gmail.com>
2015-07-08 14:32:56 -0500

    tweetextension.php

---
+++ tweetextension.php
@@ -0,0 +1,46 @@
+<?php
+require_once('graspingtweets.php');
+
+$tweet=returnTweet();
+
+$encodedtweet = urlencode($tweet);
+// echo "<br> Encoded tweet = $encodedtweet <br>";
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+
+$ar = json_decode($result);
+
+echo "<table border=\"1\" color=\"red\" style=\"width:100%\">";
+echo "<tr>";
+echo "<th> Topic:Sentiment </th>";
+echo "<th> Tweet </th>";
+echo "</tr>";
+foreach ( $ar as $k => $v )
+{
+   if ( $k == "topics")
+   {
+    echo "<tr><td>";
+    for ( $count = 0; $count < sizeof($v); $count ++ )
+    {
+      foreach ( $v[$count] as $topic => $sentiment)
+      {
+        echo "$topic : $sentiment";
+        echo "<br>";
+      }
+    }
+    echo "</td>";
+  }
+
+  if ($k == "tweet")
+  {
+    echo "<td>" . $v . "</td>";
+    echo "</tr>";
+  }
+
+}
+echo "</table>";
+echo "</body>";
+echo "</html>";
+?>
