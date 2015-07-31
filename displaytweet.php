f20b4cd257b48313ea24ce44089b89fe739a9066
Bharat Rao <bharatgit14@gmail.com>
2015-07-31 16:44:03 -0500

    displaytweet.php

--- displaytweet.php
+++ displaytweet.php
@@ -2,56 +2,8 @@
 require_once('graspingsentiment.php');
 header("Refresh:180");
 
-
-$ts = array();
-
-
-function displayTweet()
-{
-global $ts;
-$ar=sentimentReturn();
-array_push($ts, $ar);
-
-echo "<html>";
-echo "<body>";
-echo "<table border=\"1\" color=\"red\" style=\"width:100%\">";
-echo "<tr>";
-echo "<th> Topic:Sentiment </th>";
-echo "<th> Tweet </th>";
-echo "</tr>";
-for ($i=0; $i<sizeof($ts); $i++)
-{
-foreach ( $ts[$i] as $k => $v )
-{
-   if ( $k == "topics")
-   {
-    echo "<tr><td>";
-    for ( $count = 0; $count < sizeof($v); $count ++ )
-    {
-      foreach ( $v[$count] as $topic => $sentiment)
-      {
-        echo "$topic : $sentiment";
-        echo "<br>";
-      }
-    }
-    echo "</td>";
-  }
-
-  if ($k == "tweet")
-  {
-    echo "<td>" . $v . "</td>";
-    echo "</tr>";
-  }
-
-}
-}
-
-echo "</table>";
-echo "</body>";
-echo "</html>";
-}
-
-
+//ar=sentimentReturn();
+array_push($ts, sentimentReturn());
 displayTweet();
 
 
--- graspingsentiment.php
+++ graspingsentiment.php
@@ -1,6 +1,6 @@
 <?php
 require_once('graspingtweets.php');
-
+$ts = array();
 
 function sentimentReturn()
 {
@@ -18,5 +18,46 @@ $ar = json_decode($result);
 return $ar;
 }
 
+function displayTweet()
+{
+global $ts;
+echo "<html>";
+echo "<body>";
+echo "<table border=\"1\" color=\"red\" style=\"width:100%\">";
+echo "<tr>";
+echo "<th> Topic:Sentiment </th>";
+echo "<th> Tweet </th>";
+echo "</tr>";
+for ($i=0; $i<sizeof($ts); $i++)
+{
+foreach ( $ts[$i] as $k => $v )
+{
+   if ( $k == "topics")
+   {
+    echo "<tr><td>";
+    for ( $count = 0; $count < sizeof($v); $count ++ )
+    {
+      foreach ( $v[$count] as $topic => $sentiment)
+      {
+        echo "$topic : $sentiment";
+        echo "<br>";
+      }
+    }
+    echo "</td>";
+  }
+
+  if ($k == "tweet")
+  {
+    echo "<td>" . $v . "</td>";
+    echo "</tr>";
+  }
+
+}
+}
+
+echo "</table>";
+echo "</body>";
+echo "</html>";
+}
 
 ?>
