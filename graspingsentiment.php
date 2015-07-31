1fba86321705eab729ed1b20265c12835fb476b5
Bharat Rao <bharatgit14@gmail.com>
2015-07-31 16:11:05 -0500

    graspingsentiment.php

---
+++ graspingsentiment.php
@@ -0,0 +1,22 @@
+<?php
+require_once('graspingtweets.php')
+
+
+function sentimentReturn()
+{
+$tweet=tweetReturn();
+
+$encodedtweet = urlencode($tweet);
+// echo "<br> Encoded tweet = $encodedtweet <br>";
+$curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+
+$ar = json_decode($result);
+
+return $ar;
+}
+
+
+?>
