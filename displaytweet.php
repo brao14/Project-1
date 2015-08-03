e26b59a7ac3216830718503e54659b20bc06f8aa
Bharat Rao <bharatgit14@gmail.com>
2015-08-03 15:23:46 -0500

    gii

--- displaytweet.php
+++ displaytweet.php
@@ -1,8 +1,9 @@
 <?php
+session_start();
 require_once('graspingsentiment.php');
 header("Refresh:180");
 
-session_start();
+
 $_SESSION['tns'] = array();
 $ts=$_SESSION['tns'];
 
@@ -12,7 +13,7 @@ $ts=$_SESSION['tns'];
 
 $tweet=tweetReturn();
 array_push($_SESSION['tns'], $tweet);
-print_r($_SESSION);
+//print_r($_SESSION);
 
 displayTweet();
 
