31e73463e7eb4be2cf29da814bc173ab69d7c8fa
Bharat Rao <bharatgit14@gmail.com>
2015-08-03 17:55:42 -0500

    yahoo

--- showJustTweets.php
+++ showJustTweets.php
@@ -10,8 +10,13 @@ require_once('graspingtweets.php');
 <p> Hello, I will display tweets for you </p>
 <table>
 <?php
+$tw = tweetReturn();
+if ( array_pop($_SESSION['tweets'] != $tw)) {
 array_push($_SESSION['tweets'], tweetReturn());
 displayTweet();
+} else {
+  echo "No new tweets" . "<br>";
+}
 ?>
 </table>
 
