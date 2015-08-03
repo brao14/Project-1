7cb3bacd3ad5c9a8b8b65cdc4de1c705d5d0c595
Bharat Rao <bharatgit14@gmail.com>
2015-08-03 14:45:08 -0500

    okay get tweet

--- graspingsentiment.php
+++ graspingsentiment.php
@@ -7,7 +7,7 @@ function sentimentReturn()
 $tweet=tweetReturn();
 
 $encodedtweet = urlencode($tweet);
-// echo "<br> Encoded tweet = $encodedtweet <br>";
+echo "<br> Encoded tweet = $encodedtweet <br>";
 $curl = curl_init("http://apis.feeltiptop.com/demos/Restler/twitterdemo/tweet/analyze?tweet=" . $encodedtweet);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($curl);
